a36dc9b7d00e04c0a7ff6cee38000127eb384ba9
dasari810 <dasarimadhava810@gmail.com>
2020-10-24 19:34:44 +0530

    main folder deleted

--- BookmyTable/settings.py
+++ BookmyTable/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webapp'
+    'webapp',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
