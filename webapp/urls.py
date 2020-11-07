c00069bea0db6fdf263da1fe4acd1071019f72e0
dasari810 <dasarimadhava810@gmail.com>
2020-11-07 22:56:43 +0530

    guest and manager login templates added

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,4 +3,9 @@ from . import  views
 
 urlpatterns = [
     path('',views.index,name="index"),
+    path('login_guest',views.login_guest,name="guest_login"),
+    path('login_manager',views.login_manager,name="manager_login"),
+    path('list',views.res_list,name="list"),
+    path('detail',views.res_detail,name="detail"),
+    path('guest',views.guest,name="guest"),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from webapp.models import Restaurant
 # Create your views here.
 
+def index(request) :
+     return render(request,'webapp/index.html')
 
-def index(request):
-     return render(request,'webapp/restaurant_list.html')
+def login_guest(request) :
+     return render(request,'webapp/login_guest.html') 
+
+def login_manager(request) :
+     return render(request,'webapp/login_manager.html')     
+
+def res_list(request):
+     res = Restaurant.objects.all()
+     print(res)
+     return render(request,'webapp/restaurant_list.html',context={ 'res' : res })
+
+def res_detail(request):
+     return render(request,'webapp/restaurant.html')
+
+def guest(request):
+     return render(request,'webapp/guest_form.html')
+
+
